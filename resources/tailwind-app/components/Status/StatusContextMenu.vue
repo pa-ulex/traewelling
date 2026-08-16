4476e8ce880f9deb6ecbef8fcd147f67c94bdc4e
pau_lex <125640270+pa-ulex@users.noreply.github.com>
2026-08-16 21:58:44 +0200

    Update StatusContextMenu.vue

--- resources/tailwind-app/components/Status/StatusContextMenu.vue
+++ resources/tailwind-app/components/Status/StatusContextMenu.vue
@@ -19,6 +19,7 @@ import { Notyf } from 'notyf';
 import { computed, inject, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { Api, StatusResource, TripResource } from '../../../types/Api.gen';
+import { getArrivalAttribute } from '../../../vue/helpers/DateTimeHelper';
 import { useUserStore } from '../../../vue/stores/user';
 import TripCopyModal from '../Trip/TripCopyModal.vue';
 
@@ -46,7 +47,7 @@ const copyTripModalOpen = ref(false);
 const isOwn = computed(() => !!userStore.user && userStore.user.id === props.status.user.id);
 
 const canCopyTrip = computed(() => isOwn.value && !!props.status.checkin.tripUuid);
-    
+
 const connectionsLink = computed(() => {
     const dest = props.status.checkin.destination;
     const arrival = getArrivalAttribute(props.status);
@@ -199,6 +200,12 @@ async function handleBlock() {
                         <button :disabled="busyArrivalNow" @click="arrivalNow">
                             <PlaneLanding class="inline-block size-4" />
                             {{ trans('status.arrival-now') }}
+                        </button>
+                    </li>
+                    <li>
+                        <button :to="connectionsLink">
+                            <ArrowBigRightDash class="inline-block size-4" />
+                            {{ trans('status.further-connections') }}
                         </button>
                     </li>
                     <li>
